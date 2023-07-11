6920ef903443ab278a5db5eda686f370baca983d
Alberto Haboba <betoh@hotmail.com>
2023-07-11 15:37:18 -0600

    intro ok layaout 90%

--- sightsonbikes/static/sightsonbikes/script.js
+++ sightsonbikes/static/sightsonbikes/script.js
@@ -6,4 +6,69 @@ function myFunction(x) {
     } else {
       x.style.display = "block";
     }
-}
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  let currentDroppable = null;
+  const bike = document.getElementById('bike');
+
+  bike.addEventListener('mousedown', function(event) {
+    let shiftX = event.clientX - bike.getBoundingClientRect().left;
+    let shiftY = event.clientY - bike.getBoundingClientRect().top;
+
+    bike.style.position = 'absolute';
+    bike.style.zIndex = 1000;
+    document.body.append(bike);
+
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+      bike.style.left = pageX - shiftX + 'px';
+      bike.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+
+      bike.hidden = true;
+      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+      bike.hidden = false;
+
+      if (!elemBelow) return;
+
+      let droppableBelow = elemBelow.closest('.droppable');
+      if (currentDroppable != droppableBelow) {
+        if (currentDroppable) {
+          leaveDroppable(currentDroppable);
+        }
+        currentDroppable = droppableBelow;
+        if (currentDroppable) {
+          enterDroppable(currentDroppable);
+        }
+      }
+    }
+
+    function onMouseUp() {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      bike.onmouseup = null;
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  function enterDroppable(elem) {
+    elem.style.background = '#D60078';
+    window.location.href = 'home';
+  }
+
+  function leaveDroppable(elem) {
+    elem.style.background = '';
+  }
+
+  bike.ondragstart = function() {
+    return false;
+  };
+});
+
--- sightsonbikes/views.py
+++ sightsonbikes/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, "sightsonbikes/index.html");
 
+def home(request):
+    return render(request, "sightsonbikes/home.html");
+
 
