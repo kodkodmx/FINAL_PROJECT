233fd9fa7a0521bd6ac58a75e027832e9aacc85a
Alberto Haboba <betoh@hotmail.com>
2023-07-30 02:01:32 -0600

    se cambio a template

--- sightsonbikes/static/sightsonbikes/script.js
+++ sightsonbikes/static/sightsonbikes/script.js
@@ -273,11 +273,11 @@ else if (window.location.pathname == '/book') {
       document.getElementById('price').innerHTML = '$ 350 USD.';
     } else {
       document.getElementById('privatetime').style.display = 'none';
-      document.getElementById('price').innerHTML = '$ ' + 65*document.getElementById('pax').value + ' USD.';
+      document.getElementById('price').innerHTML = `\$ ${65 * document.getElementById('pax').value} USD.`;
     }};
   function price(x) {
     if (document.getElementById('price').innerHTML != '$ 350 USD.') {
-      document.getElementById('price').innerHTML = '$ ' + 65*x.value + ' USD.';
+      document.getElementById('price').innerHTML = `\$ ${65 * x.value} USD.`;
     }};
   }
 //test
