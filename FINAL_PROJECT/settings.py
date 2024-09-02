7b149186b4db435fc5632b287e0ef4df757a7c89
Alberto Haboba <betoh@hotmail.com>
2024-09-01 22:36:37 -0600

    test3

--- FINAL_PROJECT/settings.py
+++ FINAL_PROJECT/settings.py
@@ -129,7 +129,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 
 STATIC_ROOT = BASE_DIR / "static"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
